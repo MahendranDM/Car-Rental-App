dfd191baef0224de00692419f63239b651c5ced2
MahendranDM <mahendrandm2003@gmail.com>
2026-07-14 06:49:22 +0530

    Added View Cars

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 import AddCar from './components/AddCar'
+import ViewCars from './components/ViewCars'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,7 +13,7 @@ function App() {
     <>
       
            <AddCar/>
-
+<ViewCars/>
 
     </>
   )
